c1925533b32c0d8523e21a31685f9503dbbbf9ea
carlosGT72724 <carlosbt7274@gmail.com>
2024-08-31 21:00:50 -0600

    update proyectos

--- src/pages/proyectos.jsx
+++ src/pages/proyectos.jsx
@@ -1,60 +1,105 @@
-import Header from '../components/Header.jsx'
-import Footer from '../components/Footer.jsx'
-import ThreeScene from '../lib/model.jsx'
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+import ThreeScene from "../lib/model.jsx";
 
-import ecommerce from '../../public/images/ecomerce.png'
-import hr from '../../public/images/hr.jpg'
+import ecommerce from "../../public/images/ecomerce.png";
+import hr from "../../public/images/hr.jpg";
+import crud from "../../public/images/crud.png";
+import rh from "../../public/images/hr.jpg";
 
-import crud from '../../public/images/crud.png'
-import rh from '../../public/images/hr.jpg'
+import nvim from "../../public/images/nvim.png";
+import dotfiles from "../../public/images/dotfiles.png";
 
 export default function Proyectos() {
-    return (
+  return (
     <>
-            <Header />
-            <div className=' container mx-auto w-full lg:max-w-screen-md pt-14 px-4'>
-                <div className=" max-w-md mx-auto h-[calc(100vh_-_60vh)] " >
-                    <ThreeScene />
-                </div>
-                <div className="max-w-md mx-auto my-4" >
-                    <h1 className=" my-3 font-bold text-xl dark:text-white">Trabajo</h1>
-                    <div className="grid grid-cols-2 " >
-                       <div className="px-4 grid grid-rows-2 " >
-                            <div className="py-6 ">
-                                <img src={ ecommerce } alt="" />
-                                <h1 className=" dark:text-white  text-center py-2" >
-                                    Ecommerce
-                                </h1>
-                                <p className=" dark:text-white text-sm text-center ">
-                                    Plataforma de e-commerce diseñada para gestionar productos y pedidos.
-                                </p>
-                            </div>
+      <Header />
+      <div className=" container mx-auto w-full lg:max-w-screen-md pt-14 px-4">
+        <div className=" max-w-md mx-auto h-[calc(100vh_-_60vh)] ">
+          <ThreeScene />
+        </div>
+        <div className="max-w-lg mx-auto my-4">
+          <h1 className=" my-3 font-bold text-xl dark:text-white">Trabajos</h1>
 
-                            <div className="px-4" >
-                                <img src={ crud } alt="" />
-                                <h1 className="dark:text-white  text-center py-2">
-                                    Crud
-                                </h1>
-                            </div>
-                       </div> 
-                       <div className=" px-4 grid grid-rows-2 " >
-                            <div className="py-6">
-                                <img src={ hr } alt="" />
-                                <h1 className=" dark:text-white text-center py-2" >
-                                    HR system
-                                </h1>
-                                <p className=" dark:text-white text-sm text-center ">
-                                    Control de Recursos Humanos
-                                </p>
-                            </div>
-                            <div className=" px-4">
-                                <img src={ rh } alt="" />
-                            </div>
-                       </div>
-                    </div>
-                </div>
+          <div className="grid lg:grid-cols-2 gap-6">
+
+            <div className=" mb-6">
+              <div className="w-full text-center">
+                <a className=" relative cursor-pointer" href="">
+                  <img src={ecommerce} className=" rounded-xl" />
+                  <h3 className=" dark:text-white text-xl mt-2" >E-commerce</h3>
+                  <p className=" dark:text-white text-sm" >
+                    Plataforma de e-commerce diseñada para gestionar productos y pedidos.
+                  </p>
+                </a>
+              </div>
             </div>
-            <Footer />
+
+            <div className=" mb-6">
+              <div className=" w-full text-center" >
+                <a className=" relative cursor-pointer" href="">
+                  <img src={rh} className=" rounded-xl" />
+                  <h3 className=" dark:text-white text-xl mt-2">Sistema RH</h3>
+                  <p className=" dark:text-white text-sm">Control de Recursos Humanos</p>
+                </a>
+              </div>
+            </div>
+            
+
+            <div className=" mb-6">
+              <div className=" w-full text-center">
+                <a href="">
+                  <img src={crud} className=" rounded-xl" />
+                  <h3></h3>
+                  <p></p>
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="max-w-lg mx-auto my-4">
+          <h1 className=" my-3 font-bold text-xl dark:text-white">
+            Colaboraciones
+          </h1>
+          <div className=" grid lg:grid-cols-2 gap-6 ">
+
+            <div className=" mb-6">  
+              <div className=" w-full text-center">
+                <a className=" relative cursor-pointer" href="">
+                  <img src={nvim} className=" rounded-xl w-full h-auto object-cover" />
+                  <h3 className="dark:text-white text-xl mt-2">Neovim</h3>
+                  <p className="dark:text-white text-sm">
+                    Mi IDE para Neovim con configuraciones insanas. Completamente
+                    gratuito y impulsado por la comunidad.
+                  </p>
+                </a>
+              </div>
+            </div>
+
+            <div className=" mb-6">  
+              <div className=" w-full text-center">
+                <a className=" relative cursor-pointer"  href="">
+                  <img src={dotfiles} className=" rounded-xl w-full h-auto object-cover" />
+                  <h3 className=" dark:text-white text-xl mt-2">Dotfiles</h3>
+                  <p className="dark:text-white text-sm">
+                    Entorno BSPWM para distribuciones basadas en Arch
+                  </p>
+                </a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
-    )
+  );
 }
